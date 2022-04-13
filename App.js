915cbfaa92a44a6d3760a0863e85691c10442084
HPavlova <77845327+HPavlova@users.noreply.github.com>
2022-04-13 14:36:59 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,188 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import {
+  Dimensions,
+  ImageBackground,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import AppLoading from "expo-app-loading";
+// import  {AppLoading}  from "expo";
+import * as Font from "expo-font";
+
+// import RegistrationScreen from "./Screens/RegistrationScreen";
+// import LoginScreen from "./Screens/LoginScreen";
+
+const initialState = {
+  email: "",
+  password: "",
+};
+
+const loadApplication = async () => {
+  await Font.loadAsync({
+    "DMMono-Regular": require("./assets/fonts/DM_Mono/DMMono-Regular.ttf"),
+  });
+};
 
 export default function App() {
+  console.log(Platform.OS);
+  const [isShowKeyboard, setisShowKeyboard] = useState(false);
+  const [state, setstate] = useState(initialState);
+  const [isReady, setisReady] = useState(false);
+  const [dimensions, setdimensions] = useState(
+    Dimensions.get("window").width - 20 * 2
+  );
+  const image = require("./assets/images/luxfon.com-17592.jpg");
+
+  useEffect(() => {
+    const onChange = () => {
+      const width = Dimensions.get("window").width-20*2;
+      console.log(width);
+      setdimensions(width);
+    };
+    // const subscription = Dimensions.addEventListener("change", onChange);
+    Dimensions.addEventListener("change", onChange);
+    return () => {
+      // subscription?.remove();
+      Dimensions.removeEventListener("change", onChange);
+    };
+  }, []);
+
+  const keyboardHide = () => {
+    setisShowKeyboard(false);
+    Keyboard.dismiss();
+    console.log(state);
+    setstate(initialState);
+  };
+
+  if (!isReady) {
+    <AppLoading
+      startAsync={loadApplication}
+      onFinish={() => setisReady(true)}
+      onError={console.warn}
+    />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={keyboardHide}>
+      <View style={styles.container}>
+        <ImageBackground source={image} style={styles.image}>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+          >
+            <View
+              style={{
+                ...styles.form,
+                marginBottom: isShowKeyboard ? 20 : 100,
+                width: dimensions,
+              }}
+            >
+              <View style={styles.header}>
+                <Text style={styles.headerTitle}>Hello again</Text>
+                <Text style={styles.headerTitle}>Welcome back</Text>
+              </View>
+              <View>
+                <Text style={styles.inputTitle}>EMAIL</Text>
+                <TextInput
+                  style={styles.input}
+                  textAlign={"center"}
+                  onFocus={() => setisShowKeyboard(true)}
+                  value={state.email}
+                  onChangeText={(value) =>
+                    setstate((prevState) => ({ ...prevState, email: value }))
+                  }
+                />
+              </View>
+              <View style={{ marginTop: 20 }}>
+                <Text style={styles.inputTitle}>PASSWORD</Text>
+                <TextInput
+                  style={styles.input}
+                  textAlign={"center"}
+                  secureTextEntry={true}
+                  onFocus={() => setisShowKeyboard(true)}
+                  value={state.password}
+                  onChangeText={(value) =>
+                    setstate((prevState) => ({ ...prevState, password: value }))
+                  }
+                />
+              </View>
+              <View>
+                <TouchableOpacity style={styles.btn} onPress={keyboardHide}>
+                  <Text style={styles.btnTitle}>SIGN IN</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <StatusBar style="auto" />
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  form: {
+    marginHorizontal: 40,
+  },
+  input: {
+    height: 40,
+    color: "#fff",
+    borderWidth: 1,
+    borderColor: "#fff",
+    borderRadius: 6,
+  },
+  inputTitle: {
+    color: "#fff",
+    fontSize: 18,
+    marginBottom: 10,
+    fontFamily: "DMMono-Regular",
+  },
+  btn: {
+    height: 40,
+    backgroundColor: "pink",
+    borderRadius: 6,
+    borderWidth: 1,
+    marginTop: 40,
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 40,
+    ...Platform.select({
+      ios: {
+        borderColor: "pink",
+      },
+      android: {
+        borderColor: "transparent",
+      },
+    }),
+  },
+  btnTitle: {
+    color: "#fff",
+    fontSize: 18,
+  },
+  header: {
+    alignItems: "center",
+    marginBottom: 150,
+  },
+  headerTitle: {
+    fontSize: 30,
+    color: "#fff",
+    fontFamily: "DMMono-Regular",
   },
 });
