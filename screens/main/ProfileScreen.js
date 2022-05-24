cb5b0e28699214233d4b1dcd41f9e2007bed91a3
HPavlova <77845327+HPavlova@users.noreply.github.com>
2022-05-24 19:37:57 +0300

    commit ProfileScreen
    add User Posts

--- screens/main/ProfileScreen.js
+++ screens/main/ProfileScreen.js
@@ -1,19 +1,66 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { StyleSheet, View, Button, FlatList, Image } from "react-native";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  onSnapshot,
+} from "firebase/firestore";
+import { db } from "../../firebase/config";
+
+const dbFirestore = getFirestore(db);
 
 import { authSignOutUser } from "../../redux/auth/authOperations";
 
 export default function ProfileScreen() {
+  const [userPosts, setUserPosts] = useState([]);
   const dispatch = useDispatch();
+  const { userId } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    getUserPosts();
+  }, []);
+
+  const getUserPosts = async () => {
+    const q = query(
+      collection(dbFirestore, "posts"),
+      where("userId", "==", userId)
+    );
+
+    await onSnapshot(q, (data) =>
+      setUserPosts(data.docs.map((doc) => ({ ...doc.data() })))
+    );
+  };
+
   const signOut = () => {
     dispatch(authSignOutUser());
   };
 
   return (
     <View style={styles.container}>
-      <Text>ProfileScreen</Text>
-      <Button title="signOut" onPress={signOut} />
+      <Button style={styles.btn} title="signOut" onPress={signOut} />
+      <View>
+      <FlatList
+        data={userPosts}
+        keyExtractor={(item, indx) => indx.toString()}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              marginBottom: 10,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Image
+              source={{ uri: item.photo }}
+              style={{ width: 350, height: 200 }}
+            />
+          </View>
+        )}
+      ></FlatList>
+      </View>
     </View>
   );
 }
@@ -24,4 +71,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  btn: { marginTop: 50 },
 });
